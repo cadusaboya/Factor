9f53f43b67f195005c879f38c0db1aa2bb480d71
cadusaboya <carlosepsaboya@gmail.com>
2024-05-23 15:10:29 -0400

    Fixed user Cash to return the correct value

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -6,6 +6,16 @@ class User(AbstractUser):
     cpf = models.DecimalField(max_digits=11, decimal_places=0, default=0.00)
     telefone = models.DecimalField(max_digits=11, decimal_places=0, default=0.00)
 
+    def update_cash(self):
+        # Calculate the sum of values for all tasks associated with the user
+        cash_sum = Task.objects.filter(user_id=self.id).aggregate(models.Sum('value'))['value__sum']
+        # Update the cash field
+        self.cash = cash_sum if cash_sum is not None else 0
+        self.save()
+
+    def __str__(self):
+        return self.username
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     name = models.CharField(max_length=255)
@@ -13,3 +23,7 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        super(Task, self).save(*args, **kwargs)
+        self.user.update_cash()
