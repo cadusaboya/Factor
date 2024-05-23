5e12b31b853fc1f3ed1ddfc8ee357febd4ca34e6
cadusaboya <carlosepsaboya@gmail.com>
2024-05-23 11:57:38 -0400

    Added Task model for Users

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,6 +1,7 @@
 # myapp/admin.py
 
 from django.contrib import admin
-from .models import User
+from .models import User, Task
 
 admin.site.register(User)
+admin.site.register(Task)
--- server/accounts/models.py
+++ server/accounts/models.py
@@ -5,3 +5,11 @@ class User(AbstractUser):
     cash = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     cpf = models.DecimalField(max_digits=11, decimal_places=0, default=0.00)
     telefone = models.DecimalField(max_digits=11, decimal_places=0, default=0.00)
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    name = models.CharField(max_length=255)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
