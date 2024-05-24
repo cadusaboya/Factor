a8c06479061d35a6768ba85680ad621853a8a045
cadusaboya <carlosepsaboya@gmail.com>
2024-05-23 20:49:08 -0400

    Update

--- react-native/app/tabs/index.tsx
+++ react-native/app/tabs/index.tsx
@@ -1,46 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { View, ImageBackground, StyleSheet, TouchableOpacity, Dimensions, ActivityIndicator } from 'react-native';
-import { Button, ButtonGroup, withTheme, Text } from '@rneui/themed';
-import { useNavigation } from '@react-navigation/native';
-import Header from '@/components/Header';
+import { View, StyleSheet, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { ThemedText } from '@/components/ThemedText';
 import axios from 'axios';
 import { useAuth } from '@/hooks/useAuth';
 
-
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  const API_URL = 'https://factor-cadusaboya.loca.lt';
+  const { token } = useAuth(); // Retrieve the token using the useAuth hook
 
-    const navigation = useNavigation();
-    const API_URL = 'https://factor-cadusaboya.loca.lt';
-    const { token } = useAuth(); // Retrieve the token using the useAuth hook
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchUserData = async () => {
-        try {
-          const response = await axios({
-            method: 'GET',
-            url: "https://factor-cadusaboya.loca.lt/user-cash/",
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          });
-          setUserData(response.data);
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-        } finally {
-          setLoading(false);
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/user-cash/`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
         }
-      };
-    
-      fetchUserData();
-    }, [token]);
+      });
+      setUserData(response.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const handleButtonPress = (menu: string) => {
-        console.log(`Navigating to ${menu}`);
-        navigation.navigate(menu); // Navigate to the desired screen
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      // Fetch user data every time the screen gains focus
+      fetchUserData();
+    }, [])
+  );
+
+  const handleButtonPress = (menu) => {
+    console.log(`Navigating to ${menu}`);
+    navigation.navigate(menu); // Navigate to the desired screen
   };
 
   return (
@@ -54,64 +55,47 @@ export default function HomeScreen() {
         )}
       </View>
 
-
       <View style={styles.center}>
         <View style={styles.row}>
-            <TouchableOpacity
+          <TouchableOpacity
             style={styles.button}
             onPress={() => handleButtonPress('Antecipação')}>
             <Text style={styles.buttonText}>Antecipação</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+          </TouchableOpacity>
+          <TouchableOpacity
             style={styles.button}
             onPress={() => handleButtonPress('Meus Pedidos')}>
             <Text style={styles.buttonText}>Meus Pedidos</Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
         <View style={styles.row}>
-            <TouchableOpacity
+          <TouchableOpacity
             style={styles.button}
             onPress={() => handleButtonPress('Mensagens')}>
             <Text style={styles.buttonText}>Mensagens</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+          </TouchableOpacity>
+          <TouchableOpacity
             style={styles.button}
             onPress={() => handleButtonPress('Meus Hospitais')}>
             <Text style={styles.buttonText}>Meus hospitais</Text>
-            </TouchableOpacity>
-        </View>
-
-        <View style={styles.row}>
-          <TouchableOpacity
-              style={styles.button}
-              onPress={() => handleButtonPress('Registro')}>
-              <Text style={styles.buttonText}>Teste</Text>
           </TouchableOpacity>
         </View>
-
+       
       </View>
     </View>
   );
-};
-
-const { width } = Dimensions.get('window');
-const buttonWidth = (width - 32) / 2.5; // Adjust button width according to screen size
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E7E7E7',
   },
-  backgroundImage: {
-    width: '100%',
-    aspectRatio: 16 / 7, // Aspect ratio 16:9
-  },
   overlay: {
     height: 150,
     marginBottom: 50,
     backgroundColor: '#1c1b1b',
   },
-
   saldo: {
     color: 'white',
     marginTop: 50,
@@ -131,7 +115,7 @@ const styles = StyleSheet.create({
     marginBottom: 50,
   },
   button: {
-    width: buttonWidth,
+    width: 150,
     height: 150, // Adjust button height as needed
     backgroundColor: '#1c1b1b',
     justifyContent: 'center',
@@ -147,7 +131,6 @@ const styles = StyleSheet.create({
   center: {
     justifyContent: 'center', // Center content vertically
     alignItems: 'center', // Center content horizontally
-    marginVertical: 100
+    marginVertical: 100,
   },
 });
-
