4c20bcaaaa92fb62209c023c5eef8362745666be
cadusaboya <carlosepsaboya@gmail.com>
2024-05-26 15:14:26 -0400

    Removed mensagens view

--- react-native/app/AppNavigator.tsx
+++ react-native/app/AppNavigator.tsx
@@ -5,7 +5,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './tabs/index';
 import Tab2Screen from './tabs/pedidos';
 import Tab1Screen from './tabs/antecipacao';
-import Tab3Screen from './tabs/mensagens';
 import Tab4Screen from './tabs/hospitais';
 import ProfileScreen from './tabs/ProfileScreen';
 import Header from '@/components/Header';
@@ -29,7 +28,6 @@ const AppNavigator = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Antecipação" component={Tab1Screen} />
       <Stack.Screen name="Meus Pedidos" component={Tab2Screen} />
-      <Stack.Screen name="Mensagens" component={Tab3Screen} />
       <Stack.Screen name="Meus Hospitais" component={Tab4Screen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
       
