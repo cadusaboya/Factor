da59a55a0f4da9e2fc391c66ebb596a4c9f448c0
cadusaboya <carlosepsaboya@gmail.com>
2024-06-09 17:11:28 -0400

    Moving app.tsx to allow exporting to Snack

--- react-native/app.tsx
+++ react-native/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './AppNavigator';
+import AppNavigator from './app/AppNavigator';
 import { registerRootComponent } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AuthProvider } from '@/context/AuthContext';
+import { AuthProvider } from './context/AuthContext'; // Update the file path here
 import { CopilotProvider } from "react-native-copilot";
 
 export default function App() {
